d3280de02adffd8988d1c5d4a20c139b25ee807f
bluebullet07 <brain.k7@gmail.com>
2020-07-02 22:48:20 +0100

    removed error code from index file

--- visits/index.js
+++ visits/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const redis = require('redis');
-const process = require ('process');
+
 
 const app = express();
 const client = redis.createClient({
@@ -10,7 +10,7 @@ const client = redis.createClient({
 client.set('visits', 0);
 
 app.get('/', (req,res) => {
-    process.exit(10);
+    
     client.get('visits', (err, visits) => {
         res.send('Number of visits is ' + visits);
         client.set('visits', parseInt(visits) + 1);
