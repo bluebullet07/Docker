97010d615fb9edbf7539a9f3645937afdf9a34fb
bluebullet07 <brain.k7@gmail.com>
2020-07-03 22:42:09 +0100

    added test container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there all how are you ?
+          Hi there all woking fine ?
         </p>
         <a
           className="App-link"
