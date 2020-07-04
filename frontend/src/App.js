3d9fbdd21a461b42f2c40e420b8c71b855bb5a43
bluebullet07 <brain.k7@gmail.com>
2020-07-04 21:30:50 +0100

    updated js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there all woking fine ?
+          Hi there welcome to react ?
         </p>
         <a
           className="App-link"
